786a6d58612d8ccf3343618a1452a2d6d327d2a3
CeylonPark <ceylonp20@gmail.com>
2023-03-14 20:13:41 +0900

    Initial commit

---
+++ src/main/java/com/ceylon/cutter/Cutter.java
@@ -0,0 +1,17 @@
+package com.ceylon.cutter;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Cutter extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
